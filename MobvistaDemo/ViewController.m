d6c4eaa61490eba94b81223b86cb0b4fb569aa35
Acan <huangzishan123@gmail.com>
2019-03-21 15:46:12 +0800

    导入基本的静态库

---
+++ MobvistaDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MobvistaDemo
+//
+//  Created by Acan on 2019/3/21.
+//  Copyright © 2019 weiphone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
